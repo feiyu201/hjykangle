1a9ddb3e6446bfa3348a98e3c628d9ebc675198d
feiyu201 <77211066+feiyu201@users.noreply.github.com>
2022-03-16 00:00:44 +0800

    Add files via upload

---
+++ sh/xg.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+files="/root/s-hell"
+source $files/iver
+PREFIX="/vhs/kangle/ext/php"
+source $files/config
+echo "正在修改kangle中.................."
+function xg_kanglezsxsrq(){
+echo "正在修改kangle中.................."
+    wget $DOWNLOAD_URL/xgwj/xgzsxs -O /vhs/kangle/nodewww/webftp/vhost/control/index.ctl.php
+    wget $DOWNLOAD_URL/xgwj/xgymjxjl -O /vhs/kangle/nodewww/webftp/vhost/control/domain.ctl.php
+echo "修改证书到期时间,品牌,域名显示,域名解析(必须欢聚云修改的模板才生效!).................."
+}
+xg_kanglezsxsrq
+
