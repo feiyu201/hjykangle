38a420a32d499fc5484e09e2f80db411e9c54a5a
feiyu201 <77211066+feiyu201@users.noreply.github.com>
2022-04-20 14:36:25 +0800

    Update xg.sh

--- sh/xg.sh
+++ sh/xg.sh
@@ -6,8 +6,8 @@ source $files/config
 echo "正在修改kangle中.................."
 function xg_kanglezsxsrq(){
 echo "正在修改kangle中.................."
-    wget $DOWNLOAD_URL/xgwj/xgzsxs -O /vhs/kangle/nodewww/webftp/vhost/control/index.ctl.php
-    wget $DOWNLOAD_URL/xgwj/xgymjxjl -O /vhs/kangle/nodewww/webftp/vhost/control/domain.ctl.php
+    wget $DOWNLOAD_URL/xgwj1/xgzsxs -O /vhs/kangle/nodewww/webftp/vhost/control/index.ctl.php
+    wget $DOWNLOAD_URL/xgwj1/xgymjxjl -O /vhs/kangle/nodewww/webftp/vhost/control/domain.ctl.php
 echo "修改证书到期时间,品牌,域名显示,域名解析(必须欢聚云修改的模板才生效!).................."
 }
 xg_kanglezsxsrq
